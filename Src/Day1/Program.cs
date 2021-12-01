ec2d6801761510def791de4e871221680b7cb10a
Ferdi <ferdivt@deltan-n.nl>
2021-12-01 14:14:27 +0100

    Added Day1

--- Src/Day1/Program.cs
+++ Src/Day1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day1
 {
@@ -6,7 +8,41 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text1 = System.IO.File.ReadAllText(@"input-1.txt");
+            var input1 = text1.Split(Environment.NewLine).Select(i => Convert.ToInt32(i)).ToList();
+            var output1 = Test1(input1);
+            Console.WriteLine($"Result of test1 is : {output1}");
+
+            var text2 = System.IO.File.ReadAllText(@"input-2.txt");
+            var input2 = text2.Split(Environment.NewLine).Select(i => Convert.ToInt32(i)).ToList();
+            var output2 = Test2(input2);
+            Console.WriteLine($"Result of test2 is : {output2}");
         }
+
+        private static int Test1(List<int> input)
+        {
+            var count = 0;
+            foreach(var i in Enumerable.Range(1,input.Count-1))
+            {
+                if (input[i - 1] < input[i])
+                    count++;
+            }
+            return count;
+        }
+
+        private static int Test2(List<int> input)
+        {
+            var count = 0;
+            var lastValue = int.MaxValue;
+            foreach (var i in Enumerable.Range(2, input.Count-2))
+            {
+                var currentValue = input[i - 2] + input[i - 1] + input[i];
+                if (lastValue < currentValue)
+                    count++;
+                lastValue = currentValue;
+            }
+            return count;
+        }
+
     }
 }
